82a453cb0dce3c40c8a2ab85d4093bc130967ff3
David Mihal <eastsideski@gmail.com>
2014-03-15 21:16:32 -0400

    documentation for payout logic

--- client/views/dash.js
+++ client/views/dash.js
@@ -21,14 +21,20 @@ Template.dash.me = function(){
 
 Template.dash.getOwes = function(){
   var result = {};
+  // For each expense where I owe money...
   Expenses.find({owers : Meteor.userId()}).map(function (doc) {
+    // ...subtract my share of the expense
     result[doc.payer] = (result[doc.payer]|0) - (doc.ammount / doc.owers.length);
   });
+  // For each expense where I paid...
   Expenses.find({payer: Meteor.userId()}).map(function (doc) {
+    // ...and for each person that is splitting the cost...
     doc.owers.forEach(function(ower){
-      result[ower] = (result[ower]) + (doc.ammount / doc.owers.length);
+      // ...add their share of the expense
+      result[ower] = (result[ower]|0) + (doc.ammount / doc.owers.length);
     });
-  })
+  });
+  
   var objs = [];
   for(var id in result){
     if (result[id]){
