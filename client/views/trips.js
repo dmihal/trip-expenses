cf32c715b82a583928f64b8eb659b82680e66ac3
David Mihal <eastsideski@gmail.com>
2014-05-12 18:47:29 -0400

    style stuff

--- client/views/trips.js
+++ client/views/trips.js
@@ -1,9 +1,12 @@
 Template.trips.userTrips = function(){
   return Trips.find({members:Meteor.userId()});
 };
+Template.trips.memberCount = function(){
+  return this.members.length;
+}
 Template.trips.events({
   'click .tripCard' : function(e){
-  	var id = e.target.dataset.id;
+    var id = e.target.dataset.id;
     Session.set('currentTrip',id);
     history.pushState({}, "page 2", Trips.findOne(id).name);
     e.preventDefault();
